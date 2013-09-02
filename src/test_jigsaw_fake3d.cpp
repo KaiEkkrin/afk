e80c1c6125a07a6b181a33ff3d226b45a004ab64
Alex Holloway <astatine@xaqithis.com>
2013-09-02 14:41:05 +0100

    Improve the fake3d test.

--- src/test_jigsaw_fake3d.cpp
+++ src/test_jigsaw_fake3d.cpp
@@ -16,20 +16,34 @@ static void tryConvert(
     Vec3<int> realCoord = testFake.fake3DTo2D(_fakeCoord);
     Vec3<int> convertedCoord = testFake.fake3DFrom2D(realCoord);
     if (convertedCoord != _fakeCoord) std::cout << "FAILED: ";
+
+    Vec3<int> realSize = testFake.get2DSize();
+    if (realCoord.v[0] >= realSize.v[0] || realCoord.v[1] >= realSize.v[1] || realCoord.v[2] >= realSize.v[2])
+        std::cout << "FAILED: ";
+
     std::cout << _fakeCoord << " -> " << realCoord << " -> " << convertedCoord << std::endl;
 }
 
 static void tryOneJigsaw(const Vec3<int>& testSize)
 {
-    std::cout << "Testing fake 3D jigsaw: " << testSize << std::endl;
     AFK_JigsawFake3DDescriptor testFake(true, testSize);
+    Vec3<int> realSize = testFake.get2DSize();
+    std::cout << "Testing fake 3D jigsaw: " << testSize << " (Real size: " << realSize << ")" << std::endl;
 
     Vec3<int> testZero = afk_vec3<int>(0, 0, 0);
+    Vec3<int> testOneX = afk_vec3<int>(1, 0, 0);
+    Vec3<int> testOneY = afk_vec3<int>(0, 1, 0);
+    Vec3<int> testOneZ = afk_vec3<int>(0, 0, 1);
+    Vec3<int> testAllOnes = afk_vec3<int>(1, 1, 1);
     Vec3<int> testMax = testSize - afk_vec3<int>(1, 1, 1);
     tryConvert(testZero, testFake);
+    tryConvert(testOneX, testFake);
+    tryConvert(testOneY, testFake),
+    tryConvert(testOneZ, testFake);
+    tryConvert(testAllOnes, testFake);
     tryConvert(testMax, testFake);
 
-    for (int i = 0; i < 12; ++i)
+    for (int i = 0; i < 10; ++i)
     {
         Vec3<int> testVal = afk_vec3<int>(
             random() % testSize.v[0],
