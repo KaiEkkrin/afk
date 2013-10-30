e6fa611f47cd13b942ccb74d34307ca4a2f86d91
Alex Holloway <astatine@xaqithis.com>
2013-10-30 15:35:48 +0000

    Re-enable lockless claiming, edit out dodgy comments.  Right now this gets a lovely desync; I need to try flattening the polymer...

--- src/data/claimable.hpp
+++ src/data/claimable.hpp
@@ -30,28 +30,9 @@
  * re-use until the next frame.
  */
 
-/* Define this to try using a mutex instead.
- * On Linux I think this is better than the yield loop: it
- * gets slightly smoother throughput with lower CPU usage
- * (because it's not chewing system time with yield().
- * However, I should leave the option in in case on some
- * platform mutexes turn out to be really expensive...
- * (Linux uses futexes remember)
- *
- * ...
- *
- * TODO: It now turns out that I haven't maintained the
- * non-lock version, and if you don't define this it won't
- * work.
- * I should consider doing that; the lock requires me to
- * allocate every value in the polymer on the heap, whilst
- * a lockless version of this class could be trivially
- * assignable and destructable, potentially removing that
- * issue (and a possible concurrency bug associated with it:
- * occasionally I catch AFK trying to use a boost mutex
- * structure that appears uninitialised).
+/* Optionally, define this to try using a mutex instead.
  */
-#define CLAIMABLE_MUTEX 1
+#define CLAIMABLE_MUTEX 0
 
 #if CLAIMABLE_MUTEX
 #include <boost/thread/shared_mutex.hpp>
