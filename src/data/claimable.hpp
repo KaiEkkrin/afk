30153a478234dacbb1aa5faa64bb2d7809f753d7
Alex Holloway <astatine@xaqithis.com>
2013-11-24 18:19:55 +0000

    Split grabShared and returnShared out of claimable (they're more generic) and fix silly performance bug in returnShared

--- src/data/claimable.hpp
+++ src/data/claimable.hpp
@@ -27,6 +27,7 @@
 #include <boost/atomic.hpp>
 
 #include "frame.hpp"
+#include "volatile.hpp"
 
 /* A "Claimable" is a lockable thing with useful features. 
  * It can optionally
@@ -50,80 +51,6 @@
  * Actual program errors are assert()ed instead.
  */
 class AFK_ClaimException: public std::exception {};
-
-/* These utilities transfer the volatile shared structures into
- * usable things and back again.
- */
-
-template<typename T, typename I>
-void afk_grabSharedIntegral(T *mine, const volatile T *shared, size_t& offset) noexcept
-{
-    *(reinterpret_cast<I*>(mine) + offset / sizeof(I)) =
-        *(reinterpret_cast<const volatile I*>(shared) + offset / sizeof(I));
-    offset += sizeof(I);
-}
-
-template<typename T>
-void afk_grabShared(T *mine, const volatile T *shared) noexcept
-{
-    size_t offset = 0;
-    while (offset < sizeof(T))
-    {
-        switch (sizeof(T) - offset)
-        {
-        case 1:
-            afk_grabSharedIntegral<T, uint8_t>(mine, shared, offset);
-            break;
-
-        case 2: case 3:
-            afk_grabSharedIntegral<T, uint16_t>(mine, shared, offset);
-            break;
-
-        case 4: case 5: case 6: case 7:
-            afk_grabSharedIntegral<T, uint32_t>(mine, shared, offset);
-            break;
-
-        default:
-            afk_grabSharedIntegral<T, uint64_t>(mine, shared, offset);
-            break;
-        }
-    }
-}
-
-template<typename T, typename I>
-void afk_returnSharedIntegral(const T *mine, volatile T *shared, size_t& offset) noexcept
-{
-    *(reinterpret_cast<volatile I*>(shared) + offset / sizeof(I)) =
-        *(reinterpret_cast<const I*>(mine) + offset / sizeof(I));
-    offset += sizeof(I);
-}
-
-template<typename T>
-void afk_returnShared(const T *mine, volatile T *shared) noexcept
-{
-    size_t offset = 0;
-    while (offset < sizeof(T))
-    {
-        switch (offset)
-        {
-        case 1:
-            afk_returnSharedIntegral<T, uint8_t>(mine, shared, offset);
-            break;
-
-        case 2: case 3:
-            afk_returnSharedIntegral<T, uint16_t>(mine, shared, offset);
-            break;
-
-        case 4: case 5: case 6: case 7:
-            afk_returnSharedIntegral<T, uint32_t>(mine, shared, offset);
-            break;
-
-        default:
-            afk_returnSharedIntegral<T, uint64_t>(mine, shared, offset);
-            break;
-        }
-    }
-}
 
 template<typename T>
 class AFK_Claim;
