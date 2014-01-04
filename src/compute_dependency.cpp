8aa03842f2c3ab14c323426dbcb07f11baecf34a
Alex Holloway <astatine@xaqithis.com>
2014-01-04 02:01:18 +0000

    Try to fix compute_dependency so it always keeps contiguous event arrays contiguous -- doesn't work yet -- hopefully useful

--- src/compute_dependency.cpp
+++ src/compute_dependency.cpp
@@ -17,54 +17,135 @@
 
 #include "afk.hpp"
 
+#include <algorithm>
 #include <cassert>
 
 #include "compute_dependency.hpp"
 
-void AFK_ComputeDependency::clearEvents(void)
+void AFK_ComputeDependency::expandEvents(int newMaxEventCount)
 {
-    for (auto ev : e)
+    if (useEvents)
     {
-        AFK_CLCHK(computer->oclShim.ReleaseEvent()(ev))
+        /* Make a gravestone for my old, defunct self: */
+        AFK_ComputeDependency gravestone = std::move(*this);
+
+        /* Re-initialise myself */
+        events = std::make_shared<std::vector<cl_event> >(newMaxEventCount);
+        eventCount = gravestone.eventCount;
+        computer = gravestone.computer;
+        assert(useEvents);
+
+        /* Stick that stone in the graveyard */
+        graveyard.push_front(std::move(gravestone));
     }
-    e.clear();
 }
 
-void AFK_ComputeDependency::copyEvents(const std::vector<cl_event>& _e)
+void AFK_ComputeDependency::clearEvents(void)
 {
-    e.reserve(e.size() + _e.size());
-    for (auto ev : _e)
+    if (useEvents)
     {
-        AFK_CLCHK(computer->oclShim.RetainEvent()(ev))
-        e.push_back(ev);
+        /* Release everything I had */
+        for (int i = 0; i < eventCount; ++i)
+        {
+            computer->oclShim.ReleaseEvent()(events->at(i));
+            events->at(i) = 0;
+        }
+
+        /* The graveyard gets released too */
+        graveyard.clear();
     }
 }
 
 AFK_ComputeDependency::AFK_ComputeDependency(AFK_Computer *_computer):
+    eventCount(0),
     computer(_computer),
     useEvents(_computer->getUseEvents())
 {
+    if (useEvents)
+    {
+        events = std::make_shared<std::vector<cl_event> >(AFK_CDEP_SMALLEST);
+    }
 }
 
 AFK_ComputeDependency::AFK_ComputeDependency(const AFK_ComputeDependency& _dep):
+    events(_dep.events),
+    eventCount(_dep.eventCount),
     computer(_dep.computer),
     useEvents(_dep.useEvents)
 {
     if (useEvents)
     {
-        clearEvents();
-        copyEvents(_dep.e);
+        /* I need to retain all these events I just gained a reference to... */
+        for (int i = 0; i < eventCount; ++i)
+        {
+            computer->oclShim.RetainEvent()(events->at(i));
+        }
+
+        /* ...and I need to keep the graveyard, too */
+        std::copy(_dep.graveyard.begin(), _dep.graveyard.end(), graveyard.begin());
     }
 }
 
 AFK_ComputeDependency& AFK_ComputeDependency::operator=(const AFK_ComputeDependency& _dep)
 {
-    assert(computer == _dep.computer);
-    assert(useEvents == _dep.useEvents);
+    assert(_dep.useEvents == useEvents);
+
+    /* Assignment replaces whatever was here, of course */
+    clearEvents();
+
+    computer = _dep.computer;
     if (useEvents)
     {
-        clearEvents();
-        copyEvents(_dep.e);
+        /* Copy and reference everything in _dep */
+        events = _dep.events;
+        eventCount = _dep.eventCount;
+
+        for (int i = 0; i < eventCount; ++i)
+        {
+            computer->oclShim.RetainEvent()(events->at(i));
+        }
+
+        std::copy(_dep.graveyard.begin(), _dep.graveyard.end(), graveyard.begin());
+    }
+
+    return *this;
+}
+
+AFK_ComputeDependency::AFK_ComputeDependency(AFK_ComputeDependency&& _dep):
+    events(std::move(_dep.events)),
+    eventCount(_dep.eventCount),
+    computer(_dep.computer),
+    useEvents(_dep.useEvents)
+{
+    if (useEvents)
+    {
+        /* I can just invalidate the dependency, it won't
+         * do any releases on delete and all will be happy
+         */
+        _dep.eventCount = -1;
+
+        /* ...and I can move the graveyard. */
+        std::move(_dep.graveyard.begin(), _dep.graveyard.end(), graveyard.begin());
+    }
+}
+
+AFK_ComputeDependency& AFK_ComputeDependency::operator=(AFK_ComputeDependency&& _dep)
+{
+    assert(_dep.useEvents == useEvents);
+
+    /* Like the other assignment operator: */
+    clearEvents();
+
+    computer = _dep.computer;
+    if (useEvents)
+    {
+        events = std::move(_dep.events);
+        eventCount = _dep.eventCount;
+
+        /* Again, I can just invalidate _dep */
+        _dep.eventCount = -1;
+
+        std::move(_dep.graveyard.begin(), _dep.graveyard.end(), graveyard.begin());
     }
 
     return *this;
@@ -72,9 +153,42 @@ AFK_ComputeDependency& AFK_ComputeDependency::operator=(const AFK_ComputeDepende
 
 AFK_ComputeDependency& AFK_ComputeDependency::operator+=(const AFK_ComputeDependency& _dep)
 {
+    /* Make sure these are compatible */
     assert(computer == _dep.computer);
     assert(useEvents == _dep.useEvents);
-    if (useEvents) copyEvents(_dep.e);
+
+    if (useEvents)
+    {
+        assert(eventCount >= 0);
+        assert(_dep.eventCount >= 0);
+
+        int combinedEventCount = eventCount + _dep.eventCount;
+        assert(combinedEventCount >= 0);
+        if (static_cast<size_t>(combinedEventCount) > events->size())
+        {
+            /* Try to hold to the doubling pattern, rather than
+             * making a new array just big enough, which would no
+             * doubt cause me to run out of space again right away
+             */
+            int newMaxEventCount = events->size();
+            while (newMaxEventCount < combinedEventCount) newMaxEventCount *= 2;
+
+            expandEvents(newMaxEventCount);
+        }
+
+        /* Retain the other events, and splice them in... */
+        for (int i = 0; i < _dep.eventCount; ++i)
+        {
+            computer->oclShim.RetainEvent()(_dep.events->at(i));
+            events->at(eventCount + i) = _dep.events->at(i);
+        }
+
+        eventCount += _dep.eventCount;
+
+        /* ...and keep the other's graveyard */
+        graveyard.insert(graveyard.begin(), _dep.graveyard.begin(), _dep.graveyard.end());
+    }
+
     return *this;
 }
 
@@ -87,30 +201,51 @@ cl_event *AFK_ComputeDependency::addEvent(void)
 {
     if (useEvents)
     {
-        size_t oldSize = e.size();
-        e.push_back(0);
-        return &e[oldSize];
+        assert(eventCount >= 0 && static_cast<size_t>(eventCount) <= events->size());
+        if (static_cast<size_t>(eventCount) == events->size())
+        {
+            /* Make some more space. */
+            expandEvents(events->size() * 2);
+        }
+
+        assert(static_cast<size_t>(eventCount) < events->size());
+
+        /* Give back the next event. */
+        return &events->at(eventCount++);
     }
     else return nullptr;
 }
 
 cl_uint AFK_ComputeDependency::getEventCount(void) const
 {
-    if (useEvents) return static_cast<cl_uint>(e.size());
+    if (useEvents)
+    {
+        assert(eventCount >= 0);
+        return static_cast<cl_uint>(eventCount);
+    }
     else return 0;
 }
 
 const cl_event *AFK_ComputeDependency::getEvents(void) const
 {
-    if (useEvents && e.size() > 0) return e.data();
+    if (useEvents)
+    {
+        assert(eventCount >= 0);
+        return (eventCount > 0) ? events->data() : nullptr;
+    }
     else return nullptr;
 }
 
 void AFK_ComputeDependency::waitFor(void)
 {
-    if (useEvents && e.size() > 0)
+    if (useEvents)
     {
-        AFK_CLCHK(computer->oclShim.WaitForEvents()(static_cast<cl_uint>(e.size()), e.data()))
+        assert(eventCount >= 0);
+        if (eventCount > 0)
+        {
+            AFK_CLCHK(computer->oclShim.WaitForEvents()(getEventCount(), getEvents()))
+        }
+
         clearEvents();
     }
 }
--- src/compute_dependency.hpp
+++ src/compute_dependency.hpp
@@ -20,9 +20,13 @@
 
 #include "afk.hpp"
 
+#include <deque>
+#include <memory>
 #include <vector>
 
 #include "computer.hpp"
+
+#define AFK_CDEP_SMALLEST 4
 
 /* This class wraps the cl_event lists typically passed around by
  * OpenCL functions, giving a way of expressing how compute operations
@@ -33,17 +37,70 @@
 class AFK_ComputeDependency
 {
 protected:
-    std::vector<cl_event> e;
+    /* The list of events mustn't move in memory after I've used it,
+     * yet I want to be able to produce a contiguous block of all
+     * events.
+     * I resolve this problem by starting with an array of size
+     * AFK_CDEP_SMALLEST.  Every time the current array runs out
+     * of space, I shunt it aside, allocate a new array
+     * double its size *and copy the old array into it*.
+     * When all events are waited for and cleared, I can free the
+     * old array(s) (but not before).
+     */
+
+    /* The current event array is stored as a shared_ptr, so that
+     * the backing memory will survive being arbitrarily copied about.
+     * The cl_event container within is a vector because the size
+     * varies between arrays and because I want a contiguous-memory
+     * structure.
+     */
+    std::shared_ptr<std::vector<cl_event> > events;
+
+    /* The events vector size is the maximum number of events it
+     * can store, so I store the current number of events here.
+     * I need to reallocate and copy whenever this hits maximum.
+     * A negative value means that this compute dependency has been
+     * invalidated and shouldn't be used again.
+     */
+    int eventCount;
+
+    /* As dependencies outgrow their event arrays, they go into the
+     * graveyard, here, and are overwritten by new ones with larger
+     * event arrays.
+     * The graveyard can contain multiple dependencies because of
+     * the operator+= function (merge two dependencies, each of
+     * which could have graveyards).
+     */
+    std::deque<AFK_ComputeDependency> graveyard;
+
     AFK_Computer *computer;
     const bool useEvents;
 
+    /* This function chucks the current dependency into the
+     * graveyard and re-makes it with the new event count,
+     * below.
+     */
+    void expandEvents(int newMaxEventCount);
+
     void clearEvents(void);
-    void copyEvents(const std::vector<cl_event>& _e);
 
 public:
     AFK_ComputeDependency(AFK_Computer *_computer);
     AFK_ComputeDependency(const AFK_ComputeDependency& _dep);
+
+    /* This overwrites the current dependency with the supplied one,
+     * effectively dereferencing the current one.
+     */
     AFK_ComputeDependency& operator=(const AFK_ComputeDependency& _dep);
+
+    /* The move constructor and assignment operator can avoid retain
+     * calls.  Hopefully the compiler will know to use these instead
+     * sometimes :)
+     */
+    AFK_ComputeDependency(AFK_ComputeDependency&& _dep);
+    AFK_ComputeDependency& operator=(AFK_ComputeDependency&& _dep);
+
+    /* This merges two dependencies. */
     AFK_ComputeDependency& operator+=(const AFK_ComputeDependency& _dep);
 
     virtual ~AFK_ComputeDependency();
