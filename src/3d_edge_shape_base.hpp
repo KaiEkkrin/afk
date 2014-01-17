5ceb89a4bbedbbd09cf06f4745be229189b79650
Alex Holloway <astatine@xaqithis.com>
2014-01-17 21:51:04 +0000

    Fix copyright message

--- src/3d_edge_shape_base.hpp
+++ src/3d_edge_shape_base.hpp
@@ -1,5 +1,5 @@
 /* AFK
- * Copyright (C) 2013, Alex Holloway.
+ * Copyright (C) 2013-2014, Alex Holloway.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
